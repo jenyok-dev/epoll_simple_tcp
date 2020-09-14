b4085fc05db6242d233316203d12fd5dd2165bde
Eugen <developer@mego.pro>
2020-09-14 20:20:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,5 @@
 #include "main.h" 
 
-/*	ssl support - development...
-
-	#include <openssl/crypto.h>
-	#include <openssl/x509.h>
-	#include <openssl/pem.h>
-	#include <openssl/ssl.h>
-	#include <openssl/err.h>
-
-	#define CHK_NULL(x) if ((x)==NULL) exit (1)
-	#define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
-	#define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
-
-*/
-
 #define MAX_EVENTS 4096
 #define BACKLOG 1024
 #define PORT 8088
